6a5a15e3a00f99218f82cd353f950526a0ba3d69
valdi1212 <valdi1212@gmail.com>
2016-01-21 22:24:42 +0000

    Uploading work done at home - should be enough for 3% grade

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 /**
  * Created by 1212962259 on 21.1.2016.
  */
-var template = $("#itemTemplate").html();
-var renderer = Handlebars.compile(template);
-var result = renderer((function () {
-    var result = null;
-    $.getJSON("data.json", function (data) {
-        result = data;
-    })
-})());
+$("document").ready(function () {
+    var template = $("#itemTemplate").html();
+    var renderer = Handlebars.compile(template);
+    var result = renderer((function () {
+        var result = null;
+        $.getJSON("data.json", function (data) {
+            result = data;
+        })
+    }));
 
-$("#container").html(result);
+    $("#container").html(result);
+});
